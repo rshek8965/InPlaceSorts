95cddb4875eac5c76570d76b982f2a7eb543f53a
rshek8965 <rshek8965@bths.edu>
2018-11-16 11:41:59 -0500

    edited InPlaceSorts methods

--- src/com/company/InPlaceSorts.java
+++ src/com/company/InPlaceSorts.java
@@ -12,9 +12,23 @@ public class InPlaceSorts {
     public static void insertionSort(int[] list1)
     {
         //loop through the entire array, use an index i
-        //loop from index i back to the beginning of the array, use index j
-        //if element at j-1 is greater than element at j, swap them
-        //else set j to 0
+        for (int i = 0; i < list1.length; i++)
+        {
+            //loop from index i back to the beginning of the array, use index j
+            for (int j = i; i>0; j--)
+            {
+                if (list1[j-1]>list1[j])
+                {
+                    //if element at j-1 is greater than element at j, swap them
+                    swap(list1, j-1, j)
+                }
+                else
+                {
+                    //else set j to 0
+                    j=0;
+                }
+            }
+        }
     }
 
     public static void selectionSort(double[] list1)
