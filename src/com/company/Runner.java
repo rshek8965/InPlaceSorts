da2afe16fc45b147376530fe385e2eed8c70c6b5
rshek8965 <rshek8965@bths.edi>
2018-11-20 11:22:47 -0500

    edits

--- src/com/company/Runner.java
+++ src/com/company/Runner.java
@@ -4,10 +4,16 @@ public class Runner {
 
     public static void main(String[] args) {
 
-        long tine = System.nanoTime();
-        utils.bubbleSort(randIntArr);
-        time = system.nanoTime() - time;
+//        long tine = System.nanoTime();
+//        utils.bubbleSort(randIntArr);
+//        time = system.nanoTime() - time;
+//
+//        System.out.println("Time Taken: " + time);
 
-        System.out.println("Time Taken: " + time);
+        int[] arr = {1,2,3,4};
+
+        InPlaceSorts.insertionSort(arr);
+        InPlaceSorts.selectionSort(arr);
+        InPlaceSorts.bubbleSort(arr);
     }
 }
